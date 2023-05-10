af5c4fbce9b65642607c00b5650564791aec5916
LukasH0lm <lukasholm@hotmail.com>
2023-05-10 11:01:10 +0200

    Refactored classes to avoid the use of static outside singletons

--- src/main/java/com/monkeygang/mindfactorybooking/Controller/CalendarController.java
+++ src/main/java/com/monkeygang/mindfactorybooking/Controller/CalendarController.java
@@ -4,27 +4,23 @@ import com.itextpdf.text.DocumentException;
 import com.monkeygang.mindfactorybooking.DAO.BookingDAO;
 import com.monkeygang.mindfactorybooking.BookingApplication;
 import com.monkeygang.mindfactorybooking.Objects.Booking;
-import com.monkeygang.mindfactorybooking.Objects.CurrentBookingSingleton;
+import com.monkeygang.mindfactorybooking.utility.BookingLoader;
+import com.monkeygang.mindfactorybooking.utility.CalendarLoader;
 import com.monkeygang.mindfactorybooking.utility.PDFMaker;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.input.MouseButton;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.sql.SQLException;
-import java.sql.Timestamp;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.time.LocalDate;
@@ -41,42 +37,46 @@ public class CalendarController {
 
     private final BookingDAO bookingDAO = new BookingDAO();
 
-    public static Booking currentBooking;
-
-    public double startTime = 07.00;
-
     public double heightPrLabel = 0.0;
 
-    public double spacingPrLabel = 0.0;
+    private BookingLoader bookingLoader;
+
+    private CalendarLoader calendarLoader;
 
 
 
     public CalendarController() throws SQLException, IOException {
+
+        bookingLoader = new BookingLoader(this);
+        calendarLoader = new CalendarLoader(this);
+
     }
 
 
-    public void initialize() throws SQLException {
+    public void initialize() throws SQLException, IOException {
+
+
 
         //mest for at teste om ui er "agilt"
-        timeComboBox.setValue("18:00");
-        timeComboBox.getItems().add("18:00");
-        timeComboBox.getItems().add("24:00");
-        timeComboBox.onActionProperty().setValue(e -> {
-            generateCalendar(7);
+        getTimeComboBox().setValue("18:00");
+        getTimeComboBox().getItems().add("18:00");
+        getTimeComboBox().getItems().add("24:00");
+        getTimeComboBox().onActionProperty().setValue(e -> {
+            calendarLoader.generateCalendar(7);
         });
 
         // Vi sætter startdatoen til at være dagens dato
-        datePicker.setValue(LocalDate.now());
+        getDatePicker().setValue(LocalDate.now());
 
 
         // Datoen skal konveretes til date.
         // det virker bøffet det her, hvorfor konvertere vi en dato til en dato?
-        LocalDate localDate = datePicker.getValue();
+        LocalDate localDate = getDatePicker().getValue();
         Date date = Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
 
-        datePicker.onActionProperty().setValue(e -> {
+        getDatePicker().onActionProperty().setValue(e -> {
 
-            loadBookings();
+            bookingLoader.loadBookings();
 
         });
 
@@ -90,7 +90,7 @@ public class CalendarController {
         int week = cal.get(Calendar.WEEK_OF_YEAR);
 
         // vi sætter ugenummert til at være ugenummeret for den valgte dato, når programmet starter.
-        ugeLabel.setText("Uge " + week);
+        getUgeLabel().setText("Uge " + week);
 
 
         // Vi får alle bookings fra databasen, og ligger dem i listen.
@@ -107,298 +107,18 @@ public class CalendarController {
 
         double calenderStartTime = 07.00;
 
-        generateCalendar(calenderStartTime);
 
 
+        calendarLoader.generateCalendar(calenderStartTime);
 
-
-        loadBookings();
-
-    }
-
-
-    private void generateCalendar(double calenderStartTime) {
-
-
-        double calendarEndTime = Double.parseDouble(timeComboBox.getValue().replace(":", "."));
-        double timeLabelsHeight = 0.0;
-
-
-        vBoxTid.getChildren().clear();
-
-        //har lavet det om til et for loop fordi vi gør noget for hvert trin
-        //er der ikke en smartere måde at iterate i gennem labelsne?
-        for (double i = calenderStartTime; i <= calendarEndTime; i++) {
-
-            // Vi laver flere labels med tidspunkterne indtil vi når 18.00
-            Label label = new Label(df.format(i));
-
-            // Tilføjer labels til vBoxTid
-            vBoxTid.getChildren().add(label);
-
-
-
-
-        }
-
-        for (Node node : vBoxTid.getChildren()) {
-
-            if (node.getClass() == Label.class) {
-
-                Label label = (Label) node;
-
-                // Vi tilføjer højden af labelen til timeLabelsHeight, så vi kan sætte højden på hBoxCalendar og vBoxTid
-
-                label.setPrefWidth(30);
-                label.setPrefHeight(15);
-                label.setMaxWidth(label.getPrefWidth());
-                label.setMaxHeight(label.getPrefHeight());
-                label.setMinWidth(label.getPrefWidth());
-                label.setMinHeight(label.getPrefHeight());
-
-
-                Line timeLine = new Line();
-                //timeLine.setStartX(0);
-                //timeLine.setStartY(0);
-                timeLine.setEndX(hBoxCalendar.getPrefWidth());
-                timeLine.setLayoutX(hBoxCalendar.getLayoutX());
-                timeLine.setLayoutY(hBoxCalendar.getLayoutY() + timeLabelsHeight);
-                timeLine.setStroke(Color.rgb(169,169, 169));
-
-
-                calendarAnchorPane.getChildren().add(timeLine);
-
-
-                timeLabelsHeight += label.getPrefHeight();
-                timeLabelsHeight += vBoxTid.getSpacing();
-
-                heightPrLabel = label.getPrefHeight();
-
-
-
-
-
-            }
-
-        }
-
-
-        //Vi sætter højden på hBoxCalendar og vBoxTid, samt linjerne, som opdeler vores kalender.
-        // Vi minusser med vBoxTid.getSpacing(), da når der er spacing i en vBox, så bliver der tilføjet spacing efter det sidste element, og vi vil have at kalenderen slutter ved det sidste tidspunkt.
-        hBoxCalendar.setPrefHeight(timeLabelsHeight - (vBoxTid.getSpacing() + heightPrLabel));
-        hBoxCalendar.setMaxHeight(hBoxCalendar.getPrefHeight());
-        hBoxCalendar.setMinHeight(hBoxCalendar.getPrefHeight());
-
-
-        // ikke fast værdi her (15) - SW
-        vBoxTid.setPrefHeight(timeLabelsHeight - (vBoxTid.getSpacing() + heightPrLabel));
-        vBoxTid.setMaxHeight(vBoxTid.getPrefHeight());
-        vBoxTid.setMinHeight(vBoxTid.getPrefHeight());
-
-
-        hBoxLineOne.setEndY(timeLabelsHeight - (vBoxTid.getSpacing() + heightPrLabel));
-        hBoxLineTwo.setEndY(timeLabelsHeight - (vBoxTid.getSpacing() + heightPrLabel));
-        hBoxLineThree.setEndY(timeLabelsHeight - (vBoxTid.getSpacing() + heightPrLabel));
-        hBoxLineFour.setEndY(timeLabelsHeight - (vBoxTid.getSpacing() + heightPrLabel));
-        hBoxLineFive.setEndY(timeLabelsHeight - (vBoxTid.getSpacing() + heightPrLabel));
-        hBoxLineSix.setEndY(timeLabelsHeight - (vBoxTid.getSpacing() + heightPrLabel));
-
+        bookingLoader.loadBookings();
 
     }
 
 
-    private void createSingleBooking(Booking booking){
 
-        double RectangleHeight = (booking.getEndTime().getHours() - booking.getStartTime().getHours()) * (spacingPrLabel + heightPrLabel) + (booking.getEndTime().getMinutes() * (spacingPrLabel + heightPrLabel) / 60);
-        double RectangleYStartPosition = (booking.getStartTime().getHours() - startTime) * (spacingPrLabel + heightPrLabel);
 
-
-
-        Rectangle bookingRectangle = new Rectangle(50, RectangleHeight);
-        bookingRectangle.setFill(Color.RED);
-        bookingRectangle.setY(RectangleYStartPosition);
-
-        bookingInitialize(bookingRectangle, booking);
-
-
-
-        switch (booking.getStartTime().getDay()) {
-            case 1 -> paneMandag.getChildren().add(bookingRectangle);
-            case 2 -> paneTirsdag.getChildren().add(bookingRectangle);
-            case 3 -> paneOnsdag.getChildren().add(bookingRectangle);
-            case 4 -> paneTorsdag.getChildren().add(bookingRectangle);
-            case 5 -> paneFredag.getChildren().add(bookingRectangle);
-            case 6 -> paneLordag.getChildren().add(bookingRectangle);
-            case 0 -> paneSondag.getChildren().add(bookingRectangle);
-        }
-
-    }
-
-    private void createSingleBookingFixedValues(Booking booking, Timestamp bookingStartTime, Timestamp bookingEndTime){
-
-        double RectangleHeight = (bookingEndTime.getHours() - bookingStartTime.getHours()) * (spacingPrLabel + heightPrLabel) + (bookingEndTime.getMinutes() * (spacingPrLabel + heightPrLabel) / 60);
-        double RectangleYStartPosition = (bookingStartTime.getHours() - startTime) * (spacingPrLabel + heightPrLabel);
-
-        Rectangle bookingRectangle = new Rectangle(50, RectangleHeight);
-        bookingRectangle.setFill(Color.RED);
-        bookingRectangle.setY(RectangleYStartPosition);
-
-
-        bookingInitialize(bookingRectangle, booking);
-
-        switch (bookingStartTime.getDay()) {
-            case 1 -> paneMandag.getChildren().add(bookingRectangle);
-            case 2 -> paneTirsdag.getChildren().add(bookingRectangle);
-            case 3 -> paneOnsdag.getChildren().add(bookingRectangle);
-            case 4 -> paneTorsdag.getChildren().add(bookingRectangle);
-            case 5 -> paneFredag.getChildren().add(bookingRectangle);
-            case 6 -> paneLordag.getChildren().add(bookingRectangle);
-            case 0 -> paneSondag.getChildren().add(bookingRectangle);
-        }
-
-    }
-
-
-    private void createBookingMultipleDays(Booking booking){
-
-        int daysBetweenStartdateAndEndDate = booking.getEndTime().getDate() - booking.getStartTime().getDate();
-        int currentDay = booking.getStartTime().getDate();
-
-        // vi burde nok bruge endtime her fra kalenderen
-
-        //vi laver start dagen her, da vi skal have et bestemt tidspunkt, hvor dagen starter.
-        Timestamp firstDayEndTime = new Timestamp(booking.getStartTime().getYear(), booking.getStartTime().getMonth(), booking.getStartTime().getDate(), 18, 00, 00, 00 );
-
-        createSingleBookingFixedValues(booking, booking.getStartTime(), firstDayEndTime);
-
-        System.out.println("test" + booking.getStartTime());
-
-        currentDay++;
-
-        //Vi laver dagene i mellem start dato og slut dato.
-        //Vi skal ikke gå igennem denne loop, hvis der ikke er booket mere end 2 dage, da vi laver start og slut dag uden for loopen.
-        //Vi minusser med 2, da vi ikke skal tælle de 2 dage med som vi laver ude for loopen.
-        if (daysBetweenStartdateAndEndDate > 1) {
-            for (int i = 0; i <= daysBetweenStartdateAndEndDate - 2; i++) {
-
-                Timestamp fillerStart = new Timestamp(booking.getStartTime().getYear(), booking.getStartTime().getMonth(), currentDay, 7, 00, 00, 00);
-                Timestamp fillerEnd = new Timestamp(booking.getEndTime().getYear(), booking.getEndTime().getMonth(), currentDay, 18, 00, 00, 00);
-
-                createSingleBookingFixedValues(booking, fillerStart, fillerEnd);
-
-                currentDay++;
-            }
-        }
-
-
-        // vi burde nok bruge starttime her fra kalenderen
-
-        //Vi laver slutdagen her, da vi skal have bestem tidspunkt for slutningen af dagen.
-        Timestamp lastDayStartTime = new Timestamp(booking.getEndTime().getYear(), booking.getEndTime().getMonth(), currentDay, 7, 00, 00, 00 );
-
-        Timestamp lastDayEndTime = new Timestamp(booking.getEndTime().getYear(), booking.getEndTime().getMonth(), currentDay, booking.getEndTime().getHours(), booking.getEndTime().getMinutes(), booking.getEndTime().getSeconds(), booking.getEndTime().getNanos());
-
-        createSingleBookingFixedValues(booking, lastDayStartTime, lastDayEndTime);
-
-
-    }
-
-
-
-    private void loadBookings() {
-
-        spacingPrLabel = vBoxTid.getSpacing();
-
-        // Datoen skal konveretes til date. //hvorfor?
-        LocalDate localDateOnAction = datePicker.getValue();
-        Date dateOnAction = Date.from(localDateOnAction.atStartOfDay(ZoneId.systemDefault()).toInstant());
-
-        // Vi skal finde ugenummeret af datoen.
-        Calendar calOnAction = Calendar.getInstance();
-        calOnAction.setTime(dateOnAction);
-        int weekOnAction = calOnAction.get(Calendar.WEEK_OF_YEAR);
-        int yearOnAction = calOnAction.get(Calendar.YEAR);
-
-
-        ugeLabel.setText("Uge " + weekOnAction);
-
-
-        paneMandag.getChildren().clear();
-        paneTirsdag.getChildren().clear();
-        paneOnsdag.getChildren().clear();
-        paneTorsdag.getChildren().clear();
-        paneFredag.getChildren().clear();
-        paneLordag.getChildren().clear();
-        paneSondag.getChildren().clear();
-
-        for (Booking booking : allBookings) {
-
-
-            Date bookingDate = booking.getStartTime();
-
-            System.out.println(bookingDate);
-
-
-            // Vi skal finde ugenummeret af datoen.
-            Calendar calBookingDate = Calendar.getInstance();
-            calBookingDate.setTime(bookingDate);
-            int bookingDateWeek = calBookingDate.get(Calendar.WEEK_OF_YEAR);
-
-            System.out.println("bookingDateWeek " + bookingDateWeek);
-            System.out.println("WeekOnAction " + weekOnAction);
-
-
-
-            // vi ligger 1900 til, da Timestamp er underlig D:D:D:D:
-            if (bookingDateWeek == weekOnAction && booking.getStartTime().getYear() + 1900 == yearOnAction) {
-
-
-
-                if (!(booking.getStartTime().getDay() == booking.getEndTime().getDay())){
-
-                    createBookingMultipleDays(booking);
-
-                }
-                else{
-                    createSingleBooking(booking);
-
-                }
-
-
-
-            }
-
-        }
-
-
-    }
-
-
-    public void bookingInitialize(Rectangle bookingRectangle, Booking booking) {
-        bookingRectangle.setOnMouseClicked(event -> {
-            CurrentBookingSingleton.getInstance().setCurrentBooking(booking);
-
-
-            if (event.getButton() == MouseButton.PRIMARY) {
-                if (event.getClickCount() == 2) {
-
-                    CurrentBookingSingleton.getInstance().setCurrentBooking(booking);
-
-                    CurrentBookingSingleton.getInstance().setIsEdit(true);
-
-                    loadBookingUI();
-
-
-                }
-            }
-
-
-        });
-
-
-    }
-
-    private void loadBookingUI() {
+    public void loadBookingUI() {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(BookingApplication.class.getResource("view/booking-view.fxml"));
             Parent root = fxmlLoader.load();
@@ -415,14 +135,14 @@ public class CalendarController {
 
     public void previousWeekButtonPressed() {
 
-        datePicker.setValue(datePicker.getValue().minusDays(7));
+        getDatePicker().setValue(getDatePicker().getValue().minusDays(7));
 
 
     }
 
     public void nextWeekButtonPressed() {
 
-        datePicker.setValue(datePicker.getValue().plusDays(7));
+        getDatePicker().setValue(getDatePicker().getValue().plusDays(7));
     }
 
     public void createPDF() throws DocumentException, IOException {
@@ -430,15 +150,18 @@ public class CalendarController {
     }
 
     @FXML
-    AnchorPane calendarAnchorPane;
+    private AnchorPane calendarAnchorPane;
+
+
 
     @FXML
     private DatePicker datePicker;
 
     @FXML
     private Label ugeLabel;
+
     @FXML
-    private HBox hBoxCalendar;
+    private HBox HBoxCalendar;
 
     @FXML
     private Line hBoxLineFive;
@@ -495,4 +218,79 @@ public class CalendarController {
     private ComboBox<String> timeComboBox;
 
 
+    public AnchorPane getCalendarAnchorPane() {
+        return calendarAnchorPane;
+    }
+
+    public DatePicker getDatePicker() {
+        return datePicker;
+    }
+
+    public Label getUgeLabel() {
+        return ugeLabel;
+    }
+
+    public HBox getHBoxCalendar() {
+        return HBoxCalendar;
+    }
+
+    public Line gethBoxLineFive() {
+        return hBoxLineFive;
+    }
+
+    public Line gethBoxLineFour() {
+        return hBoxLineFour;
+    }
+
+    public Line gethBoxLineOne() {
+        return hBoxLineOne;
+    }
+
+    public Line gethBoxLineSix() {
+        return hBoxLineSix;
+    }
+
+    public Line gethBoxLineThree() {
+        return hBoxLineThree;
+    }
+
+    public Line gethBoxLineTwo() {
+        return hBoxLineTwo;
+    }
+
+    public Pane getPaneFredag() {
+        return paneFredag;
+    }
+
+    public Pane getPaneLordag() {
+        return paneLordag;
+    }
+
+    public Pane getPaneMandag() {
+        return paneMandag;
+    }
+
+    public Pane getPaneOnsdag() {
+        return paneOnsdag;
+    }
+
+    public Pane getPaneSondag() {
+        return paneSondag;
+    }
+
+    public Pane getPaneTirsdag() {
+        return paneTirsdag;
+    }
+
+    public Pane getPaneTorsdag() {
+        return paneTorsdag;
+    }
+
+    public VBox getvBoxTid() {
+        return vBoxTid;
+    }
+
+    public ComboBox<String> getTimeComboBox() {
+        return timeComboBox;
+    }
 }
